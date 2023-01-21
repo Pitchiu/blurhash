948a0b4583ac88aed00e730eeab2075de86e426e
Pitchiu <kocitypu@gmail.com>
2023-01-21 17:41:38 +0100

    Comment and readme
    Comments added and readme updated

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# blurhash
+# blurhash algorithm
+This project is blurhash implementation in NVIDIA CUDA
+
+## Tests
+
+### Configuratrion
+
+*CPU*: Intel Xeon E5649 2.53 GHz
+*GPU*: NVIDIA TESLA K40
+
+Samples - 10 jpg images of size up to 1024 x 1024.
+
+### Results
+Components:
+- 1x1   (extra small)   - GPU: 441ms, CPU: 1830ms,   GPU 4x faster
+- 5x5   (typical case)  - GPU: 523ms, CPU: 34977ms,  GPU 67x faster
+- 10x10 (detailed blur) - GPU: 798ms, CPU: 139836ms, GPU 175x faster
--- src/gpu/encode_device.cu
+++ src/gpu/encode_device.cu
@@ -10,10 +10,15 @@ void encodeDevice(const ProgramOptions& options)
 	int yComponents = options.yComponents;
 
 	int acCount = xComponents * yComponents - 1;
+
+	// Some memory allocations can be once per every file because their size is equal for
+	// all iterations
+
+	// Memory allocation for AC device buffer
 	float* deviceAc;
 	CHECK_CUDA_ERROR(cudaMalloc(&deviceAc, 3*acCount*sizeof(float)));
-
 	
+	// Memory allocation for encoded AC elements
 	char* encodedACValuesBuffer;
 	CHECK_CUDA_ERROR(cudaMalloc(&encodedACValuesBuffer, sizeof(char)*2*acCount));
 
@@ -37,6 +42,8 @@ const char* encodeFile(int xComponents, int yComponents, const char* filename, f
 {
 	int width, height, channels;
 
+	// Image loading to cpu memory, this can be optimized by using gpu loading library
+	// Image is load in size 3 channels RGB AoS
 	unsigned char* data = stbi_load(filename, &width, &height, &channels, 3);
 	if (!data) return NULL;
 
@@ -48,12 +55,15 @@ const char* encodeFile(int xComponents, int yComponents, const char* filename, f
 
 const char* encodePixels(uint8_t* data, int xComponents, int yComponents, int width, int height, float* deviceAc, char *encodedACValuesBuffer)
 {
+	// Result string buffer declaration
 	static char buffer[2 + 4 + (9 * 9 - 1) * 2 + 1];
 
 	float factors[yComponents * xComponents * 3];
 
 	int N = width*height;
 	uint8_t* pixels;
+
+	// Copying .jpg data to device memory
 	CHECK_CUDA_ERROR(cudaMalloc(&pixels, N*3*sizeof(uint8_t)));
 	CHECK_CUDA_ERROR(cudaMemcpy(pixels, data, N*3*sizeof(uint8_t), cudaMemcpyHostToDevice));
 
@@ -61,6 +71,7 @@ const char* encodePixels(uint8_t* data, int xComponents, int yComponents, int wi
 	float* vec_g;
 	float* vec_b;
 
+	// Allocating 3 vectors for every channel, they are reused for every component calculation
 	CHECK_CUDA_ERROR(cudaMalloc((void**)&vec_r, sizeof(float)*N));
 	CHECK_CUDA_ERROR(cudaMalloc((void**)&vec_g, sizeof(float)*N));
 	CHECK_CUDA_ERROR(cudaMalloc((void**)&vec_b, sizeof(float)*N));
@@ -75,6 +86,7 @@ const char* encodePixels(uint8_t* data, int xComponents, int yComponents, int wi
 		}
 	}
 
+	// Free allocated memory
 	CHECK_CUDA_ERROR(cudaFree(vec_r));
 	CHECK_CUDA_ERROR(cudaFree(vec_g));
 	CHECK_CUDA_ERROR(cudaFree(vec_b));
@@ -87,6 +99,8 @@ const char* encodePixels(uint8_t* data, int xComponents, int yComponents, int wi
 	char* ptr = buffer;
 
 	int sizeFlag = (xComponents - 1) + (yComponents - 1) * 9;
+
+	// Encoding first char - size
 	ptr = encodeInt(sizeFlag, 1, ptr);
 	float maximumValue;
 	if (acCount > 0)
@@ -95,10 +109,14 @@ const char* encodePixels(uint8_t* data, int xComponents, int yComponents, int wi
   		CHECK_CUDA_ERROR(cudaMemcpy(deviceAc, ac, 3*acCount*sizeof(float), cudaMemcpyHostToDevice));
 
 		thrust::device_ptr<float> deviceAcPtr(deviceAc);
+
+		// Finding AC component with the biggest absolute value
 		actualMaximumValue = fabsf(*(thrust::max_element(deviceAcPtr, deviceAcPtr + 3*acCount, compare())));
 
 		int quantisedMaximumValue = fmaxf(0, fminf(82, floorf(actualMaximumValue * 166 - 0.5)));
 		maximumValue = ((float)quantisedMaximumValue + 1) / 166;
+
+		// Encoding second char - quantised max value
 		ptr = encodeInt(quantisedMaximumValue, 1, ptr);
 	}
 	else
@@ -107,9 +125,12 @@ const char* encodePixels(uint8_t* data, int xComponents, int yComponents, int wi
 		ptr = encodeInt(0, 1, ptr);
 	}
 
+	// Encoding chars 3-7 - DC
 	ptr = encodeInt(encodeDC(dc[0], dc[1], dc[2]), 4, ptr);
 
 	int numblocks = acCount / 128 + 1;
+
+	// Parallel AC components encoding
 	encodeACValues<<<numblocks, 128>>>(deviceAc, encodedACValuesBuffer, maximumValue, acCount);
 	cudaDeviceSynchronize();
   	CHECK_CUDA_ERROR(cudaMemcpy(buffer + 6, encodedACValuesBuffer, 2*acCount*sizeof(char), cudaMemcpyDeviceToHost));
@@ -125,6 +146,8 @@ void calculateFactor(uint8_t* pixels, int xComponent, int yComponent, int width,
 	int N = width * height;
 
 	int numblocks = N/1024 + 1;
+
+	// Every thread is processing RGB value for single pixel and saves results to correct vec
 	calculatePixelColors<<<numblocks, 1024>>>(pixels, vec_r, vec_g, vec_b, xComponent, yComponent, width, height);
 	cudaDeviceSynchronize();
 
@@ -132,6 +155,7 @@ void calculateFactor(uint8_t* pixels, int xComponent, int yComponent, int width,
 	thrust::device_ptr<float> g_ptr(vec_g);
 	thrust::device_ptr<float> b_ptr(vec_b);
 
+	// Using thrust library to sum vec for every channel
 	r = thrust::reduce(r_ptr, r_ptr + N, (float) 0);
 	g = thrust::reduce(g_ptr, g_ptr + N, (float) 0);
 	b = thrust::reduce(b_ptr, b_ptr + N, (float) 0);
